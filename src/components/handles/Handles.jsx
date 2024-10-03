cf857b9d83161f53e61d512463c39b92603eb06c
alok2424 <22ec059@jssaten.ac.in>
2024-10-03 17:27:56 +0530

    commit

---
+++ src/components/handles/Handles.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Handles.css'
+import { AiFillLinkedin, AiFillTwitterSquare } from 'react-icons/ai';
+import { FaGithubSquare } from 'react-icons/fa';
+
+const Handles = () => {
+  return (
+    <section className='socials'>
+      <a href="https://www.linkedin.com/in/alok-kumar-gupta-2075b5257/" target='_blank' rel="noopener noreferrer"><AiFillLinkedin className='icon' /></a>
+      <a href="https://twitter.com/Alok679166" target='_blank' rel="noopener noreferrer"><AiFillTwitterSquare className='icon' /></a>
+      <a href="https://github.com/alok2424" target='_blank' rel="noopener noreferrer"><FaGithubSquare className='icon' />
+      </a>
+
+
+    </section>
+  )
+}
+
+export default Handles
